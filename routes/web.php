b5d705a049f7ebc0eb32827474602960a7721407
Bithi-84 <tehzeebbithi.2016@gmail.com>
2025-06-21 20:54:59 +0600

    home update

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,8 @@
 <?php
 
+use App\Http\Controllers\FrontendController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\HomeController;
 
 
-Route::get('/',[HomeController::class,'index']);
-Route::get('/contact-me',[HomeController::class,'contact']);
+
+Route::get('/',[FrontendController::class,'index']);
